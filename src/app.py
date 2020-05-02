5f84d3b978d5f4bb908489605074982aae7d342f
johnrohan87 <john.rohan87@gmail.com>
2020-05-02 18:21:43 +0000

    files update

--- src/app.py
+++ src/app.py
@@ -10,3 +10,25 @@ API_HOST = os.getenv("API_HOST")
 API_KEY = os.getenv("API_KEY")
 
 # continue with your application here
+#print(API_HOST)
+#print(API_KEY)
+
+input_term = input("What term do you want to look for? You can make multiple requests by seperating your input with commas.")
+
+url = "https://mashape-community-urban-dictionary.p.rapidapi.com/define"
+
+headers = {
+    'x-rapidapi-host': "mashape-community-urban-dictionary.p.rapidapi.com",
+    'x-rapidapi-key': "e3115142camsh5f670bda2cc09cdp13d620jsn1218432bf1ec"
+    }
+
+querystring = {"term":input_term}
+#print(querystring)
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+body = response.json()
+dict_layer1 = body["list"]
+for items in dict_layer1:
+    print(items["definition"])
+#print(dict_layer1)
